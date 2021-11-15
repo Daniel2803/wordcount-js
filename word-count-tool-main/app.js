88e7835e3c939c5cd0aefed3fc9258759751cdaf
Daniel2803 <49502893+Daniel2803@users.noreply.github.com>
2021-11-16 10:03:04 +1300

    Add files via upload

---
+++ word-count-tool-main/app.js
@@ -0,0 +1,10 @@
+
+
+
+
+document.getElementById("btn").addEventListener('click', function(){
+  let string = document.getElementById("str").value;
+  let num = string.length;
+
+  document.getElementById("output").innerHTML = num;
+})
